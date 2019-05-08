0d8bf019f5bf6241e1c7b51bd9dd2d1144cd6a46
Balthild <ibalthild@gmail.com>
2019-05-08 22:32:16 +0800

    Click actions for navigation items

--- src/layouts/UserDashboard.js
+++ src/layouts/UserDashboard.js
@@ -14,41 +14,69 @@ import MainNavigation from '../components/MainNavigation';
 import BrandMark from '../components/BrandMark';
 import { url } from '../utils';
 
-const Nav = () => (
-    <MainNavigation dark={true} header={BrandMark}>
-        <AkNavigationItem
-            icon={<TrayIcon />}
-            text={'Activities'}
-            isSelected={false} />
-        <AkNavigationItem
-            icon={<ReposIcon />}
-            text={'Repositories'}
-            isSelected={false} />
-        <AkNavigationItem
-            icon={<GroupIcon />}
-            text={'Organizations'}
-            isSelected={false} />
+type Props = HasChildren & {
+    defaultSelectedNavItem: string;
+    onNavItemSelected?: (string) => void;
+}
 
-        <AkNavigationItem
-            icon={<IssuesIcon />}
-            text={'Issues'}
-            isSelected={false}
-            href={url('/issues')} />
-        <AkNavigationItem
-            icon={<PullRequestsIcon />}
-            text={'Pull Requests'}
-            isSelected={false}
-            href={url('/pulls')} />
-        <AkNavigationItem
-            icon={<DiscoverIcon />}
-            text={'Explore'}
-            isSelected={false}
-            href={url('/explore/repos')} />
-    </MainNavigation>
-);
+type State = {
+    selectedNavItem: string;
+}
 
-export default class UserDashboard extends React.Component<HasChildren> {
+export default class UserDashboard extends React.Component<Props, State> {
+    state = {
+        selectedNavItem: this.props.defaultSelectedNavItem,
+    };
+
+    onNavItemClicked = (selected: string) => {
+        this.setState({ selectedNavItem: selected });
+
+        if (this.props.onNavItemSelected)
+            return this.props.onNavItemSelected(selected);
+    };
+
+    renderNav() {
+        return (
+            <MainNavigation dark={true} header={BrandMark}>
+                <AkNavigationItem
+                    icon={<TrayIcon />}
+                    text={'Activities'}
+                    isSelected={this.state.selectedNavItem === 'act'}
+                    href={url('/#act')}
+                    onClick={() => this.onNavItemClicked('act')} />
+                <AkNavigationItem
+                    icon={<ReposIcon />}
+                    text={'Repositories'}
+                    isSelected={this.state.selectedNavItem === 'repo'}
+                    href={url('/#repo')}
+                    onClick={() => this.onNavItemClicked('repo')} />
+                <AkNavigationItem
+                    icon={<GroupIcon />}
+                    text={'Organizations'}
+                    isSelected={this.state.selectedNavItem === 'org'}
+                    href={url('/#org')}
+                    onClick={() => this.onNavItemClicked('org')} />
+
+                <AkNavigationItem
+                    icon={<IssuesIcon />}
+                    text={'Issues'}
+                    isSelected={this.state.selectedNavItem === 'iss'}
+                    href={url('/issues')} />
+                <AkNavigationItem
+                    icon={<PullRequestsIcon />}
+                    text={'Pull Requests'}
+                    isSelected={this.state.selectedNavItem === 'pr'}
+                    href={url('/pulls')} />
+                <AkNavigationItem
+                    icon={<DiscoverIcon />}
+                    text={'Explore'}
+                    isSelected={this.state.selectedNavItem === 'exp'}
+                    href={url('/explore/repos')} />
+            </MainNavigation>
+        );
+    }
+
     render() {
-        return <Page navigation={<Nav />}>{this.props.children}</Page>;
+        return <Page navigation={this.renderNav()}>{this.props.children}</Page>;
     }
 }
--- src/user/dashboard/Dashboard.js
+++ src/user/dashboard/Dashboard.js
@@ -3,11 +3,58 @@ import * as React from 'react';
 
 import UserDashboard from '../../layouts/UserDashboard';
 
-export default class Dashboard extends React.Component<*> {
+function getDefaultSelectedNavItem() {
+    const hash = location.hash.substr(1);
+
+    if (hash in ['act', 'repo', 'org'])
+        return hash;
+
+    return 'act';
+}
+
+const ActivitiesPage = () => (
+    <div>Activities</div>
+);
+
+const RepositoriesPage = () => (
+    <div>TODO: repo</div>
+);
+
+const OrganizationsPage = () => (
+    <div>TODO: org</div>
+);
+
+type State = {
+    currentPage: string;
+}
+
+export default class Dashboard extends React.Component<{}, State> {
+    state = {
+        currentPage: getDefaultSelectedNavItem(),
+    };
+
+    onNavItemSelected = (selected: string) => {
+        this.setState({
+            currentPage: selected,
+        });
+    };
+
+    renderPage() {
+        if (this.state.currentPage === 'org') {
+            return <OrganizationsPage />;
+        } else if (this.state.currentPage === 'repo') {
+            return <RepositoriesPage />;
+        } else {
+            return <ActivitiesPage />;
+        }
+    }
+
     render() {
         return (
-            <UserDashboard>
-                123213
+            <UserDashboard
+                defaultSelectedNavItem={getDefaultSelectedNavItem()}
+                onNavItemSelected={this.onNavItemSelected}>
+                {this.renderPage()}
             </UserDashboard>
         );
     }
